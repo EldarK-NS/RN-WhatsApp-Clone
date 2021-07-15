6d910c7fedf147ed6a30ffff686b608b44f85557
EldarK-NS <eldark.ns@gmail.com>
2021-07-15 13:19:24 +0600

    implement API and create user

--- App.tsx
+++ App.tsx
@@ -1,20 +1,71 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
-import Amplify from "aws-amplify";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
 import config from "./src/aws-exports";
 Amplify.configure(config);
 import { withAuthenticator } from "aws-amplify-react-native";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
+
+const randomImages = [
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg",
+  "https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg",
+];
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const getRandomImage = () => {
+    return randomImages[Math.floor(Math.random() * getRandomImage.length)];
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      //get auth user from Auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      // console.log(userInfo);
+      if (userInfo) {
+        //get the user from Backend with the user SUB from Auth
+        const userData = await API.graphql(
+          graphqlOperation(getUser, {
+            id: userInfo.attributes.sub,
+          })
+        );
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in DB");
+          return;
+        }
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: userInfo.username,
+          imageUri: getRandomImage(),
+          status: "Hey, i am using WhatsApp",
+        };
+
+        console.log((newUser))
+        await API.graphql(graphqlOperation(createUser,{
+           input:newUser
+        }))
+
+        
+        //if no user in DB with the Id? then create it
+      }
+    };
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
