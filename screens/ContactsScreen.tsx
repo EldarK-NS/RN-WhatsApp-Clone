d824bb1f09f8a77f2b28be0497cec91815e5a42b
EldarK-NS <eldark.ns@gmail.com>
2021-07-15 13:59:50 +0600

    get Users from DB and display it

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,16 +1,30 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, FlatList, Text, View } from "react-native";
-import Users from "../data/Users";
+// import Users from "../data/Users";
 import { NewMessageButton } from "../components/NewMessageButton/index";
 import { ContactListItem } from "../components/ContactListItem";
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../src/graphql/queries";
 
 export default function ContactsScreen() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(graphqlOperation(listUsers));
+        setUsers(usersData.data.listUsers.items);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchUsers();
+  }, []);
   return (
     <View style={styles.container}>
       <FlatList
         showsVerticalScrollIndicator={false}
         style={{ width: "100%" }}
-        data={Users}
+        data={users}
         renderItem={({ item }) => <ContactListItem user={item} />}
         keyExtractor={(item) => item.id}
       />
