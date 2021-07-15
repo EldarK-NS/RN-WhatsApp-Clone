eb3c48ace9c14e7617d8d92031311d1521f178fd
EldarK-NS <eldark.ns@gmail.com>
2021-07-15 10:46:14 +0600

    auth installed

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports'
-Amplify.configure(config)
+import Amplify from "aws-amplify";
+import config from "./src/aws-exports";
+Amplify.configure(config);
+import { withAuthenticator } from "aws-amplify-react-native";
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -26,3 +27,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
